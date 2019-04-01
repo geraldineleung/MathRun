f15ba1a0cf48c1382821afd1d5a7e6f436dfff4c
Geraldine Leung <geraldine.leung2@gmail.com>
2019-04-01 13:26:23 -0400

    fixed score on quit

--- background/quit_button.gd
+++ background/quit_button.gd
@@ -1,6 +1,8 @@
 extends Button
+onready var info = get_node("/root/new_script")
 
 func _on_quit_button_pressed():
 	var game_settings = get_node("/root/game_settings")
 	game_settings.current_scene = "Title"
+	info.score_current = 0
 	get_tree().change_scene("res://menu/title_screen/title_screen.tscn")
