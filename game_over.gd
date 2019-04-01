8e86e586cace5f15cfd2cb005726fa8477613f14
Emily Popovic <epopovic@uwo.ca>
2019-04-01 12:13:40 -0400

    changed game over screen

--- game_over.gd
+++ game_over.gd
@@ -1,6 +1,20 @@
 extends CanvasLayer
 
 onready var info = get_node("/root/new_script")
+
+func _ready():
+	
+	var lab = get_node("Score")
+	lab.set_text("Score: " + str(info.score_current))
+	
+	
+	var file = File.new()
+	file.open("res://game/Highscores.txt", file.READ_WRITE)
+	file.seek_end()
+	file.store_string("Player\n"+ str(info.score_current) +"\n")
+	file.close()
+	
+
 func _on_quit_button_pressed():
 	get_tree().change_scene("res://menu/title_screen/title_screen.tscn")
 
