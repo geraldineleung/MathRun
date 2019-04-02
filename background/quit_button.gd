38a8ac38ae40bf7d161531eb1dbe5f3edeab5814
Emily Popovic <epopovic@uwo.ca>
2019-04-02 16:03:18 -0400

    saved highscore to file when user quit

--- background/quit_button.gd
+++ background/quit_button.gd
@@ -4,5 +4,13 @@ onready var info = get_node("/root/new_script")
 func _on_quit_button_pressed():
 	var game_settings = get_node("/root/game_settings")
 	game_settings.current_scene = "Title"
+	
+	#save highscore
+	var file = File.new()
+	file.open("res://game/Highscores.txt", file.READ_WRITE)
+	file.seek_end()
+	file.store_string("\n" + str(game_settings.username) + "\n"+ str(info.score_current))
+	file.close()
+	
 	info.score_current = 0
 	get_tree().change_scene("res://menu/title_screen/title_screen.tscn")
